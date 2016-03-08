28ac1d916b37e99c67735ad8167a2a581462228a
Marcle Rodrigues <maarclee@gmail.com>
2016-03-07 22:33:12 -0300

    Ruby Fundamentals 3

---
+++ Ruby Fundamentals/word_counter.rb
@@ -0,0 +1,39 @@
+class WordCounter
+  attr_reader :file, :hash
+  def initialize file_name
+    @file = File.read(file_name)
+    @hash = {}
+  end
+
+  def words
+    file.split
+  end
+
+  def count
+    words.length
+  end
+
+  def uniq_count
+    words.uniq.length
+  end
+
+  def frequency
+    words.each do |word|
+      if hash.has_key? word
+        hash[word.to_sym] += 1
+      else
+        hash[word.to_sym] = 1
+      end
+    end
+    hash
+  end
+
+end
+
+#Test
+
+text = WordCounter.new "read_file.txt"
+
+puts text.count
+puts text.uniq_count
+puts text.frequency
